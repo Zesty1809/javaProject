3186e28ee0e2fc5ffdebdbed306e8f492a6f4a3b
Zesty <adi8116315284@gmail.com>
2024-02-02 00:01:05 +0530

    Adding gallons to liter table

---
+++ ForDemo/src/GalToLitTable.java
@@ -0,0 +1,25 @@
+/*
+    This program displays a conversion
+    table of gallons to liters.
+ */
+class GalToLitTable {
+    public static void main(String[] args) {
+        double gallons, liters;
+        int counter;
+
+        counter = 0;
+        for (gallons = 1; gallons < 101; gallons++)
+        {
+            liters = gallons * 3.7854;
+            System.out.println(gallons + " gallons is " + liters + " liters.");
+
+            counter++;
+
+            if (counter == 10)
+            {
+                System.out.println();
+                counter = 0;    // Reset counter
+            }
+        }
+    }
+}
